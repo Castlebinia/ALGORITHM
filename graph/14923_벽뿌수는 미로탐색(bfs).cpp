92b0bfffaffce288077bbe3f738b9eb7507024b9
Castlebinia <tjdqls328123@gmail.com>
2018-04-29 09:28:35 +0900

    claor123

--- graph/14923_벽뿌수는 미로탐색(bfs).cpp
+++ graph/14923_벽뿌수는 미로탐색(bfs).cpp
@@ -1,55 +1,87 @@
 #include <iostream>
-#include <string>
-#include <stack>
+#include <queue>
+#include <algorithm>
 
 using namespace std;
 
-string text;
-
-int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(NULL);
-	stack<char> left, right;
-	cin >> text;
-	int len = text.length();
-	for (int i = 0; i <len; i++) {
-		left.push(text[i]);
+class Point{
+public:
+	int x, y, crush;
+	Point(int ix, int iy, int icrush) {
+		x = ix;
+		y = iy;
+		crush = icrush;
 	}
-	int num;
-	cin >> num;
-	char n, in;
-	while (num--) {
-		cin >> n;
-		switch (n) {
-		case 'L':
-			if (!left.empty()) {
-				right.push(left.top());
-				left.pop();
-			}
-			break;
-		case 'D':
-			if (!right.empty()) {
-				left.push(right.top());
-				right.pop();
-			}
-			break;
+};
 
-		case 'B':
-			if (!left.empty())left.pop();
-			break;
-		case 'P':
-			cin >> in;
-			left.push(in);
-			break;
+int n, m;
+bool fin;
+bool visit[1000][1000][2] = { false, };
+int dx[] = { 0,0,1,-1 };
+int dy[] = { 1,-1,0,0 };
+int a[1000][1000];
+int ans[1000][1000][2];
 
+void bfs(int startx,int starty,int goalx,int goaly) {
+	queue<Point> q;
+	q.push(Point(startx, starty, 1));
+	visit[startx][starty][1] = true;
+	while (!q.empty()) {
+		int x = q.front().x;
+		int y = q.front().y;
+		if (x == goalx - 1 && y==goaly-1) {
+			cout << max(ans[x][y][0],ans[x][y][1]) << '\n';
+			fin = true;
+			return;
+		}
+		int crush = q.front().crush;
+		q.pop();
+		if (crush == 1) {
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+					if (!visit[nx][ny][1]) {
+						if (a[nx][ny] == 0) {
+							visit[nx][ny][1] = true;
+							q.push(Point(nx, ny, 1));
+							ans[nx][ny][1] = ans[x][y][1] + 1;
+						}
+						else if (a[nx][ny] == 1) {
+							visit[nx][ny][0] = true;
+							q.push(Point(nx, ny, 0));
+							ans[nx][ny][0] = ans[x][y][1] + 1;
+						}
+					}
+				}
+			}
+		}
+		else if (crush == 0) {
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+					if (!visit[nx][ny][0] && a[nx][ny] == 0) {
+						visit[nx][ny][0] = true;
+						q.push(Point(nx, ny, 0));
+						ans[nx][ny][0] = ans[x][y][0] + 1;
+					}
+				}
+			}
 		}
 	}
-	while (!left.empty()) {
-		right.push(left.top());
-		left.pop();
+}
+
+
+int main() {
+	int sx, sy, gx, gy;
+	cin >> n >> m;
+	cin >> sx >> sy >> gx >> gy;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> a[i][j];
+		}
 	}
-	while (!right.empty()) {
-		cout << right.top();
-		right.pop();
-	}
+	bfs(sx-1,sy-1,gx,gy);
+	if (!fin)cout << -1 << '\n';
 }
