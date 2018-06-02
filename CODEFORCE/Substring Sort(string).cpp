25b16a15256abac436660692151ce50f7ad0ba08
Castlebinia <tjdqls328123@gmail.com>
2018-06-02 16:01:29 +0900

    claor123

---
+++ CODEFORCE/Substring Sort(string).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	vector<pair<int, string>>v(n);
+	for (int i = 0; i < n; i++) {
+		string s;
+		cin >> s;
+		v[i] = { s.length(),s };
+	}
+	sort(v.begin(), v.end());
+	for (int i =n-1 ; i>0; i--) {
+		if (v[i].second.find(v[i - 1].second) == string::npos) {
+			cout << "NO\n";
+			return 0;
+		}
+	}
+	cout << "YES\n";
+	for (int i = 0; i < n; i++) {
+		cout << v[i].second << '\n';
+	}
+}
