0690e9aa96705c51e12b927ac1cb140bd215ed8f
Castlebinia <tjdqls328123@gmail.com>
2018-06-12 17:06:06 +0900

    claor123

---
+++ BOJ/dp/11060_점프 점프.cpp
@@ -0,0 +1,40 @@
+#include <iostream>#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int a[1002];
+int d[1102];
+
+int main() {
+	int n;
+	cin >> n;
+	d[1] = 1;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+	for (int i = 1; i <= n; i++) {
+		if (d[i] == 0)continue;	//can not
+		for (int j = 1; j <= a[i];j++) {
+			if (d[i + j] == 0)d[i + j] = d[i] + 1;
+			else d[i + j] = min(d[i + j], d[i] + 1);
+		}
+	}
+	if (d[n] == 0)cout << -1 << '\n';
+	else cout << d[n]-1 << '\n';
+}
+	int cnt = 0;
+	for (int i = 1; i <= n; i++) {
+		if (i == c || i == h)continue;
+		if (!check2[i]) {
+			cret = 0;
+			d[i]=dfs(i)+1;
+			cnt++;
+		}
+	}
+	sort(d, d + n,cmp);
+	for (int i = 0; i < k; i++) {
+		ret += d[i];
+	}
+	cout << ret << '\n';
+}
