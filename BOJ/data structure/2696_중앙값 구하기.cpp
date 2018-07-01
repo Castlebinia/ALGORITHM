df8bd35d35d86434850c6c8d4c36545e2e165996
Castlebinia <tjdqls328123@gmail.com>
2018-07-01 19:06:16 +0900

    claor123

---
+++ BOJ/data structure/2696_중앙값 구하기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <functional>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    priority_queue<int,vector<int>,greater<int>>minq;
+    priority_queue<int>maxq;
+    
+
+}
