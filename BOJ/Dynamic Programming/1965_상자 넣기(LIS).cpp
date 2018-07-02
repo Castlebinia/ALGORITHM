7c764a0a159ccb0aaa4a8470fe2b5510a07fb5d3
Castlebinia <tjdqls328123@gmail.com>
2018-07-02 14:32:07 +0900

    claor123

--- BOJ/Dynamic Programming/1965_상자 넣기(LIS).cpp
+++ BOJ/Dynamic Programming/1965_상자 넣기(LIS).cpp
@@ -15,7 +15,6 @@ int main() {
 	for (int i = 0; i < n; i++)cin >> a[i];
 	for (int i = 0; i < n; i++)d[i] = 1;
 	for (int i = 0; i < n; i++) {
-		d[i] = 1;
 		for (int j = 0; j < i; j++) {
 			if (a[i] > a[j])d[i] = max(d[i],d[j]+1);
 		}
