4fac90f667c634b82f8fc16be0fa08dd3cdf0984
Castlebinia <tjdqls328123@gmail.com>
2018-07-05 02:47:29 +0900

    claor123

---
+++ BOJ/Dynamic Programming/1309_동물원.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int mod = 9901;
+
+int d[100001][3];
+
+int main() {
+	d[0][0] = 1;
+	d[0][1] = 1;
+	d[0][2] = 1;
+
+	int n;
+	cin >> n;
+	for (int i = 1; i < n; i++) {
+		d[i][0] = (d[i - 1][0] + d[i - 1][1] + d[i - 1][2])%mod;
+		d[i][1] = (d[i - 1][0] + d[i - 1][2])%mod;
+		d[i][2] = (d[i - 1][0] + d[i - 1][1])%mod;
+	}
+	cout << (d[n-1][0] + d[n-1][1] + d[n-1][2])%mod << '\n';
+}
