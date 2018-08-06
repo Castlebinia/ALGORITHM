b4a50c74ee9925566e8c27d26a91f597e36938e0
Castlebinia <tjdqls328123@gmail.com>
2018-08-06 12:59:19 +0900

    TREE

--- BOJ/graph/1068.cpp
+++ BOJ/graph/1068.cpp
@@ -3,17 +3,14 @@
 
 using namespace std;
 
-bool check[51];
 int parent[51];
 int root;
 vector<vector<int>>child;
 
-void go(int now) {
-	check[now] = true;
-	for (auto next : child[now]) {
-		check[next] = true;
-		go(next);
-	}
+int go(int now) {
+	int ret = child[now].empty() ? 1 : 0;
+	for (auto next : child[now]) ret += go(next);
+	return ret;
 }
 
 int main() {
@@ -33,11 +30,6 @@ int main() {
 	}
 	int d;
 	cin >> d;
-	go(d);
-	int ret = 0;
-	if (child[parent[d]].size() == 1)ret++;
-	for (int i = 0; i < n; i++) {
-		if (!check[i] && !child[i].size())ret++;
-	}
-	cout << ret << '\n';
+	cout << go(root)-go(d)+(child[parent[d]].size()==1?1:0) << '\n';
+	
 }
